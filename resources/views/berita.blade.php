ceb8ccb266a01345e8ca713ac67bea57901a2a58
Karel <karelriyan@gmail.com>
2025-05-28 22:05:35 +0700

    page berita

--- resources/views/berita.blade.php
+++ resources/views/berita.blade.php
@@ -6,8 +6,72 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <div class="section-title">
-                        <h2>Informasi Terkini</h2>
+                    <div class="section-title-judul-berita">
+                        <h2>SELEKSI PPAN & PPAP TINGKAT
+                            KOTA TANGGERANG SELATAN
+                            TAHUN 2025</h2>
+                    </div>
+                    <h3 style="margin-bottom: 30px; font-size: medium;">25/06/2024, 08:45 WIB</h3>
+                    <p style="text-align: justify; font-size: medium;">
+                        KOMPAS.com - Pemerintah Indonesia melalui Badan Siber dan Sandi Negara (BSSN) akhirnya
+                        mengungkap penyebab gangguan Pusat Data Nasional (PDN) yang terjadi sejak Kamis (20/6/2024)
+                        pekan lalu.<br><br>
+
+                        Kepala BSSN Hinsa Siburian mengatakan bahwa gangguan tersebut disebabkan oleh serangan
+                        ransowmare bernama brain cipher.<br><br>
+
+                        "Perlu kami sampaikan bahwa insiden pusat data sementara ini adalah serangan siber dalam bentuk
+                        ransomware, dengan nama Brain Cipher ransomware," kata Hinsa dalam konferensi pers yang
+                        disiarkan di YouTube Kemkominfo TV. <br><br>
+
+                        Ransomware sendiri adalah program jahat (malware) yang dapat mengunci data di komputer dengan
+                        enkripsi. Pelaku penyebar ransomware lalu akan memeras korban dengan meminta tebusan dalam
+                        jumlah tertentu untuk membuka kunci tersebut. <br><br>
+
+                        Menurut Direktur Network dan IT Solutions Telkom Herlan Wirjanako, ransomware Brain Cipher ini
+                        mengunci data PDN dan meminta tebusan sebesar 8 juta dollar AS (sekitar Rp 131,2 miliar).<br><br>
+
+                        "Jadi memang di dark web (situs gelap) itu ada jalan dan kita ikuti, mereka (pihak yang menyebar
+                        ransomware) minta tebusan ada 8 juta dollar AS. Demikian," kata Herlan. <br><br>
+
+                        Investigasi menyeluruh<br><br>
+
+                        Kini, BSSN bersama Kemkominfo, Cyber Crime Kepolisian RI (Polri), dan KSO Telkomsigma masih
+                        terus berproses mengupayakan investigasi secara menyeluruh.<br><br>
+
+                        "Masih mengupayakan investigasi secara menyeluruh pada bukti-bukti forensik yang didapat, dengan
+                        segala keterbatasan barang bukti. Karena kondisinya barang bukti terenkripsi. Ini juga harus
+                        kami pecahkan," lanjut Hinsa.<br><br>
+
+                        Selain investigasi, tim gabungan BSSN masih berupaya mengatasi dampak dari serangan tersebut,
+                        termasuk memulihkan data yang terkunci serta layanan publik terdampak.<br><br>
+
+                        “Upaya-upaya ke sana sudah kami rumuskan dan kami diskusikan tadi, sehingga diharapkan bisa
+                        dengan cepat masalah ini, kejadian ini bisa diatasi dengan baik,” kata Hinsa.<br><br>
+
+                        Hinsa juga melaporkan kemajuan upaya yang dilakukan pemerintah per Senin (24/6/2024) pagi
+                        ini.<br><br>
+
+                        Ia mengatakan bahwa layanan imigrasi yang terdampak sudah beroperasi dengan normal, mulai dari
+                        layanan visa dan izin tinggal, layanan paspor, hingga layanan manajemen dokumen
+                        keimigrasian.<br><br>
+
+                        "Ini sudah berjalan, walaupun nantinya tetap akan dilakukan evaluasi-evaluasi berikutnya," ujar
+                        Hinsa sebagaimana dikutip KompasTekno dari YouTube Kemkominfo TV.<br><br>
+
+                        Diberitakan sebelumnya, gangguan sistem pada PDN Kemenkominfo berdampak terhadap layanan
+                        keimigrasian di seluruh Indonesia pada Kamis (20/6/2024).<br><br>
+
+                        Menkominfo Budi Arie Setiadi mengatakan, sejumlah layanan publik terdampak akibat kerusakan
+                        sistem pada PDN.<br><br>
+                    </p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section-title-berita">
+                        <h2>Berita Lainnya</h2>
                     </div>
                 </div>
             </div>
